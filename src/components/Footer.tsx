fd9d2449a47b6fc6056075eba7bec2e034f8d9cf
abelMarkos2023 <abelmarkos2023@gmail.com>
2025-06-16 00:09:41 +0300

    footer adjustment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center p-4 bg-black text-white mt-6">
-      <p>&copy; 2025 MangaVerse. All rights reserved.</p>
+      <p>All rights reserved <span className="text-yellow-300 text-xl">&copy;theObnoxiousTwins</span>  {new Date().getFullYear()}.</p>
     </footer>
   );
 }
